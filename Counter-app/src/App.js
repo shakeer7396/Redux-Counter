fd4b391c6485fc3c2efdf0d137dbaeacd0ff99ee
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-07-29 07:44:57 +0530

    s1

---
+++ Counter-app/src/App.js
@@ -0,0 +1,25 @@
+
+import React,{useState} from 'react';
+import { store } from './Redux/store';
+import {handleIncrement,handleDecrement} from './Redux/action';
+
+function App() {
+  const {dispatch}=store;
+  const {counter}=store.getState();
+  const [local,setLocal]=useState(0)
+  //It will run the callback function ,whenever the state inside the Redux store changes
+  store.subscribe(()=>{
+    setLocal((prev)=> prev+1);
+  });
+
+  return (
+    <div>
+      <h1>Counter : {counter}</h1>
+      <button onClick={()=>dispatch(handleIncrement())}>INCREMENT</button>
+      <button onClick={()=>dispatch(handleDecrement())}>DECREMENT</button>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Counter-app/src/Redux/reducer.js
@@ -0,0 +1,12 @@
+const reducer =(oldState,action) => {
+    switch(action.type){
+        case "INCREMENT" :
+            return {...oldState, counter:oldState.counter +action.payload};
+            case "DECREMENT":
+                return {...oldState, counter:oldState.counter - action.payload};
+                default:
+                    return oldState;
+    };
+};
+
+export {reducer}
